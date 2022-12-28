e14be0f613bd509df3f2ab0329429c76a2ff6d51
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-12-27 21:55:01 -0500

    created App and Character components to fetch Star Wars characters from the API and render character names and information to the DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [ characters, setCharacters ] = useState(characters);
+  const [ data, setData ] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -13,13 +14,21 @@ const App = () => {
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
       .then(res => {
-        setCharacters(res.characters);
+        console.log(res.data);
+        setData(res.data);
       }).catch(err => console.error(err));
   }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className='container'>
+        <div>
+          {data.map(item => {
+          return <Character key={item.id} data={item} />
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React, { Fragment, useState } from "react";
+// import styled from "styled-components";
+
+ export default function Character({data}) {
+    const [ open, setOpen ] = useState(false);
+    return (
+        <div className='characters-container'>
+            <h3>{data.name}</h3>
+            <button onClick={() => setOpen(!open)}>See Details</button>
+            {open && (
+                <Fragment>
+                    <p>{data.birth_year}</p>
+                    <p>{data.gender}</p>
+                    <p>{data.height}</p>
+                    <p>{data.mass}</p>
+                    <p>{data.hair_color}</p>
+                    <p>{data.eye_color}</p>
+                    <p>{data.skin_color}</p>
+                </Fragment>
+            )}
+        </div>
+    )
+}
